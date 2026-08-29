f133c177fc1640172e27645bbaac4461fe7dadbd
Mattias Mellin <mm225vh@student.lnu.se>
2026-08-29 16:16:58 +0200

    Add user input and print out

--- app/src/main/java/L1/App.java
+++ app/src/main/java/L1/App.java
@@ -8,5 +8,21 @@ public class App {
 
     public static void main(String[] args) {
 
+        System.out.print("Hello, what is your name? ");
+
+        String value = input.nextLine().toUpperCase();
+
+        String[][] letters = new String[value.length()][];
+
+        for (int i = 0; i < value.length(); i++) {
+            letters[i] = AsciiAlphabet.getLetter(value.charAt(i));
+        }
+
+        for (int row = 0; row < 8; row++) {
+            for (int letter = 0; letter < letters.length; letter++) {
+                System.out.printf("%-15s", letters[letter][row]);
+            }
+            System.out.println();
+        }
     }
 }
