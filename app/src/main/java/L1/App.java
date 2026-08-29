54084ff377d400ba58d181421439260c79a78ec2
Mattias Mellin <mm225vh@student.lnu.se>
2026-08-29 16:16:30 +0200

    Add ASCII library

---
+++ app/src/main/java/L1/App.java
@@ -0,0 +1,12 @@
+package L1;
+
+import java.util.Scanner;
+
+public class App {
+
+    private static Scanner input = new Scanner(System.in, "UTF-8");
+
+    public static void main(String[] args) {
+
+    }
+}
